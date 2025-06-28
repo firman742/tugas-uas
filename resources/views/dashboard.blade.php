e3171607c74a347e9e9f96dd7f297f9680b8b712
Nizar07 <nizarbasalamah8@gmail.com>
2025-06-28 11:19:19 +0700

    ganti h1 dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,10 @@
 <x-app-layout>
     @section('content')
         <div class="container mt-4">
-            <h1>Selamat Datang, {{ auth()->user()->name }}</h1>
+            <h1>Selamat Datang Di Bank Sampah, {{ auth()->user()->name }}</h1>
 
             <a href="" class="btn btn-primary">
-                Lihat Laporan Harian & Bulanan 
-                Awang NUB
+                Lihat Laporan Harian & Bulanan
             </a>
         </div>
     @endsection
